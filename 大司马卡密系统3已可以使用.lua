21e3d16ede84e3fbd1b7feb40adca725fe872b63
whenheet <3579175593@qq.com>
2025-12-09 18:14:23 +0800

    Update 大司马卡密系统3已可以使用.lua

--- 大司马卡密系统3已可以使用.lua
+++ 大司马卡密系统3已可以使用.lua
@@ -89,7 +89,7 @@ Button.MouseButton1Click:Connect(function()
        ["O3P5A7S9"] = {user = "999hj08", expire = "2030-12-31"},
        ["D2F4G6H8"] = {user = "dasima", expire = "2030-12-31"},
        ["J1K3L5M7"] = {user ="CN_ylwhnb", expire = "2030-12-31"},
-       ["N9B7V5C3"] = {user = "用户名", expire = "2030-12-31"},
+       ["N9B7V5C3"] = {user = "iiiii11111iiiii6", expire = "2030-12-31"},
        ["X8Z6D4F2"] = {user = "用户名", expire = "2030-12-31"},
        ["X1Z6D4k6"] = {user = "用户名", expire = "2030-12-31"},
        ["X8h6D4F0"] = {user = "用户名", expire = "2030-12-31"},

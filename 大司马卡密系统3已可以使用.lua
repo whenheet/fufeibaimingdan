ec33bca3c96b9d0a1c8f81fdce640091681a25e6
whenheet <3579175593@qq.com>
2025-12-09 20:01:17 +0800

    Update 大司马卡密系统3已可以使用.lua

--- 大司马卡密系统3已可以使用.lua
+++ 大司马卡密系统3已可以使用.lua
@@ -91,7 +91,7 @@ Button.MouseButton1Click:Connect(function()
        ["J1K3L5M7"] = {user ="CN_ylwhnb", expire = "2030-12-31"},
        ["N9B7V5C3"] = {user = "iiiii11111iiiii6", expire = "2030-12-31"},
        ["X8Z6D4F2"] = {user = "hvgjcigvjg", expire = "2030-12-31"},
-       ["X1Z6D4k6"] = {user = "用户名", expire = "2030-12-31"},
+       ["X1Z6D4k6"] = {user = "byhgvgyhu", expire = "2030-12-31"},
        ["X8h6D4F0"] = {user = "用户名", expire = "2030-12-31"},
        ["h5Z6D4D2"] = {user = "用户名", expire = "2030-12-31"}
         -- 可复制上面的格式，添加更多卡密
